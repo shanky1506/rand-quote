c11f15d4d1be72fbc11ab31dbbb994b9f8ec0d70
shanky1506 <shashankgunda68@outlook.com>
2020-08-30 00:55:37 +0530

    addede two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteBoxComponent } from './components/quote-box/quote-box.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteBoxComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
