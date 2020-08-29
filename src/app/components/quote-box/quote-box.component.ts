59c79a517ae96f786925c77bb951c97a8fc04882
shanky1506 <shashankgunda68@outlook.com>
2020-08-30 01:40:35 +0530

    basic quote

--- src/app/components/quote-box/quote-box.component.ts
+++ src/app/components/quote-box/quote-box.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-box.component.css']
 })
 export class QuoteBoxComponent implements OnInit {
-
+  quote = "Et tu Brute"
+  author = "Julius Caeser"
   constructor() { }
 
   ngOnInit(): void {
+  }
+  //
+  handleClick(){
+
   }
 
 }
